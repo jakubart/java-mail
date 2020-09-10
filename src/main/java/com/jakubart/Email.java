9a5e1692fdf45378512a516e75abb8d2df853738
bjakubowski <bartakj141@wp.pl>
2020-09-10 21:22:28 +0200

    add method sendMessage

--- src/main/java/com/jakubart/Email.java
+++ src/main/java/com/jakubart/Email.java
@@ -1,6 +1,14 @@
 package com.jakubart;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class Email {
 
@@ -46,5 +54,41 @@ public class Email {
         }
         System.exit(-3);
         return "Brak zamówień na liście";
+    }
+
+    public static void sendMessage(int idZam) {
+        try (InputStream input = new FileInputStream("src/main/application.properties")) {
+            Properties prop = new Properties();
+            prop.put("mail.smtp.host", "smtp.wp.pl");
+            prop.put("mail.smtp.auth", "true");
+
+            prop.load(input);
+            Session session = Session.getInstance(prop,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(prop.getProperty("email.login"), prop.getProperty("email.password"));
+                        }
+                    });
+
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(prop.getProperty("email.adress")));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(prop.getProperty("email.to")));
+                message.setSubject("Nowe zamóweinia w systemie XPRINTI");
+                message.setText(Email.writeTextMessage(Order.readOrders(idZam)));
+                Transport.send(message);
+                System.out.println("Wiadomość wysłana");
+            } catch (MessagingException e) {
+                System.out.println("Error send message");
+                e.printStackTrace();
+                System.exit(-4);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("FIle not found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("IO Exception");
+            e.printStackTrace();
+        }
     }
 }
