4cbc5f2a499c1e49afc1b1f39895332aca078166
bjakubowski <bartakj141@wp.pl>
2020-09-10 21:20:04 +0200

    add field textMessage, getter and setter

--- src/main/java/com/jakubart/Email.java
+++ src/main/java/com/jakubart/Email.java
@@ -1,4 +1,18 @@
 package com.jakubart;
 
 public class Email {
+
+    private String textMessage;
+
+    public Email(String textMessage) {
+        this.textMessage = textMessage;
+    }
+
+    public String getTextMessage() {
+        return textMessage;
+    }
+
+    public void setTextMessage(String textMessage) {
+        this.textMessage = textMessage;
+    }
 }
