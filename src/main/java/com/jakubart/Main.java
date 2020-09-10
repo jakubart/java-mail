0e0ee5e285e16d5d177370275fb76a55808d7345
bjakubowski <bartakj141@wp.pl>
2020-09-10 21:36:36 +0200

    Crete new Counter object in main class, send message and write answer in console line

--- src/main/java/com/jakubart/Main.java
+++ src/main/java/com/jakubart/Main.java
@@ -2,8 +2,20 @@ package com.jakubart;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        counter.setMysqlCunter(counter.checkMysqlCounter());
+        counter.setIdZamXprinti(counter.chcecIdZamSqlServer());
+        if (counter.getIdZamXprinti() != counter.getMysqlCunter()) {
+            Email.sendMessage(counter.getMysqlCunter());
+            System.out.println(Email.writeTextMessage(Order.readOrders(counter.getMysqlCunter())));
+            System.out.println("Zamówienia wysłane");
+            Thread.sleep(5000);
+        } else {
+            System.out.println("Nie ma nowych zamówień");
+            Thread.sleep(5000);
+            System.exit(0);
+        }
+        counter.updateCounter(counter.getIdZamXprinti());
     }
 }
