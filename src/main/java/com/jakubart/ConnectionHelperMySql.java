2e82cacee8d2259c8c6fa6a76992b2022ccb570c
bjakubowski <bartakj141@wp.pl>
2020-09-10 20:26:55 +0200

    add static method getConnection

--- src/main/java/com/jakubart/ConnectionHelperMySql.java
+++ src/main/java/com/jakubart/ConnectionHelperMySql.java
@@ -1,4 +1,29 @@
 package com.jakubart;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionHelperMySql {
+
+    private static String url = "jdbc:mysql://localhost:3306/jakosc?serverTimezone = ECT";
+    private static String user = "root";
+    private static String passwd = "admin";
+    private static String dbDriver = "com.mysql.cj.jdbc.Driver";
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(dbDriver);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Problem ze sterownikiem" + e.getMessage());
+        }
+        try {
+            conn = DriverManager.getConnection(url, user, passwd);
+        } catch (SQLException e) {
+            System.out.println("Nie udało się nawiązać połącznia" + e.getMessage());
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
